9353a35474efac87bea8900a8f3b4431a475e872
Gabrielx <47926200+gabzxth@users.noreply.github.com>
2019-10-08 19:33:57 -0300

    still working on the main page lol

---
+++ codenew - Copy/js/script.js
@@ -0,0 +1,10 @@
+function menu(){
+    var x = document.getElementById("nav");
+    if(x.style.display == "none"){
+        x.style.display = "block";
+        x.style.opacity = "100%";
+    }else{
+    x.style.display = "none";
+    }
+    
+}
